d7a13f33d5354ae83344e07981fc6ea242061849
musho7-dev <129147695+musho777@users.noreply.github.com>
2025-11-03 20:35:30 +0400

    bug fix

--- src/screens/Profile/EditProfilScreen/components/position_profession.js
+++ src/screens/Profile/EditProfilScreen/components/position_profession.js
@@ -1,27 +1,43 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
-import { DownArrow, Otrastel } from "../../../../assets/svg/Svgs"
-import { useState } from "react"
-import { Styles } from "../../../../styles/Styles"
-import { AppColors } from "../../../../styles/AppColors"
-import { Position_professionModal } from "./position_professionModal"
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {DownArrow, Otrastel} from '../../../../assets/svg/Svgs';
+import {useState} from 'react';
+import {Styles} from '../../../../styles/Styles';
+import {AppColors} from '../../../../styles/AppColors';
+import {Position_professionModal} from './position_professionModal';
 
-export const Position_profession = ({ setLocation, loaction }) => {
-  const [city, setCity] = useState(false)
-  return <View>
-    <TouchableOpacity activeOpacity={1} onPress={() => setCity(true)} style={[styles.textWrapper2, { paddingVertical: 15, justifyContent: "space-between" }]}>
-      <View style={{ flexDirection: "row", justifyContent: "center" }}>
-        <View style={{ width: 20, marginRight: 10 }}>
-          <Otrastel />
+export const Position_profession = ({setLocation, loaction}) => {
+  const [city, setCity] = useState(false);
+  return (
+    <View>
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => setCity(true)}
+        style={[
+          styles.textWrapper2,
+          {paddingVertical: 15, justifyContent: 'space-between'},
+        ]}>
+        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <View style={{width: 20, marginRight: 10}}>
+            <Otrastel />
+          </View>
+          <Text style={[Styles.balihaiRegular14, {color: '#000'}]}>
+            {loaction ? loaction : 'Сфера/отрасль'}
+          </Text>
         </View>
-        <Text style={[Styles.balihaiRegular14,{color:"#000"}]}>{loaction ? loaction : 'Сфера/отрасль'}</Text>
+        <DownArrow />
+      </TouchableOpacity>
+      <View style={{position: 'absolute'}}>
+        {city && (
+          <Position_professionModal
+            onPress={e => setLocation(e)}
+            close={() => setCity(false)}
+            visible={city}
+          />
+        )}
       </View>
-      <DownArrow />
-    </TouchableOpacity>
-    <View style={{ position: 'absolute' }}>
-      {city && <Position_professionModal onPress={(e) => setLocation(e)} close={() => setCity(false)} visible={city} />}
     </View>
-  </View>
-}
+  );
+};
 const styles = StyleSheet.create({
   img: {
     width: 80,
@@ -41,4 +57,3 @@ const styles = StyleSheet.create({
     height: 50,
   },
 });
-
